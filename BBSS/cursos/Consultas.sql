1266de767f77c427a1b176a85e04f771fedf172b
israelrg <israelrodriguez@protonmail.com>
2021-03-06 19:41:05 +0100

    Añadir consultas a la carpeta cursos ademas de pequeñas modificaciones con las constraint

---
+++ BBSS/cursos/Consultas.sql
@@ -0,0 +1,4 @@
+use cursos;
+insert into alumnos(nombre, apellido1, apellido2, dni, direccion, sexo, fechanac) values('Ricardo', 'Perez', 'Alvarez', 78334598,'Ramonycajal', 'H', '1999-02-15');
+insert into alumnos(nombre, apellido1, apellido2, dni, direccion, sexo, fechanac) values('Perez', 'Alfonso', 'benitez', 74489311 ,'La casa de la esquina', 'H', '1999-02-15');
+select * from alumnos;
